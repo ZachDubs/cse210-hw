3b880313677b48f55223b6bea3a4da8f89701617
ZachDubs <web21002@byui.edu>
2023-04-22 20:52:53 -0600

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.Write("You passed!");
+        }
+        else
+        {
+            Console.Write("You did not pass :'(");
+        }
+
     }
 }

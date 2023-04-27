72ea9713ca872c732c54f35f1d5843768657e05c
ZachDubs <web21002@byui.edu>
2023-04-26 18:56:38 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessInteger = -1;
+
+        while (guessInteger != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessInteger = int.Parse(Console.ReadLine());
+
+            if (guessInteger > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessInteger < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
